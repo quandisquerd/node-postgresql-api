d17aab384718553694414a9124696e96f76f2a33
quan1234 <quanvdph19781@fpt.edu.vn>
2023-08-16 02:56:28 +0700

    update

--- controllers/music.js
+++ controllers/music.js
@@ -2,7 +2,7 @@ const postgre = require('../database')
 const musicController = {
     getAll: async (req, res) => {
         try {
-            const { rows } = await postgre.query("select * from books")
+            const { rows } = await postgre.query("select * from musics")
             res.json({ msg: "OK", data: rows })
         } catch (error) {
             res.json({ msg: error.msg })
--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require("express")
 const app = express()
-
 require('dotenv').config()
-
 app.use(express.json())
-
-
 const musicRouter = require('./routes/music')
-
 app.use("/api", musicRouter)
-
 app.listen(process.env.PORT, () => console.log("Server is running on port 5000"))
