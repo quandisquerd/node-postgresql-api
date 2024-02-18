bc21ae607d29750bafb423bdfb96923dc9686d15
quandisquerd <97234017+quandisquerd@users.noreply.github.com>
2024-02-18 22:14:29 +0700

    Update music.js

--- controllers/music.js
+++ controllers/music.js
@@ -2,13 +2,27 @@ const postgre = require('../database')
 
 const musicController = {
      getAll: async (req, res) => {
-        try {
-            const { rows } = await postgre.query("select * from musics")
-            res.json({ msg: "OK", data: rows })
-        } catch (error) {
-            res.json({ msg: error.msg })
+    try {
+        const countQuery = await postgre.query("SELECT COUNT(*) FROM musics");
+        const totalCount = countQuery.rows[0].count;
+        let randomIndexes = [];
+        const maxLimit = 10
+        for (let i = 0; i < totalCount; i++) {
+            randomIndexes.push(i);
         }
-    },
+        for (let i = randomIndexes.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [randomIndexes[i], randomIndexes[j]] = [randomIndexes[j], randomIndexes[i]];
+        }
+        const selectedIndexes = randomIndexes.slice(0, maxLimit);
+        const whereCondition = selectedIndexes.map(index => `index_column = ${index}`).join(" OR ");
+        const { rows } = await postgre.query(`SELECT * FROM musics WHERE ${whereCondition}`);
+        
+        res.json({ msg: "OK", data: rows });
+    } catch (error) {
+        res.json({ msg: error.msg });
+    }
+},
     add: async (req, res) => {
         const { name, image, file, album_id } = req.body;
         try {
